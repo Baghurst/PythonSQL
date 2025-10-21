b5161b14f5007a779e880573cf6dd1add53c9f9c
Baghurst <23bbaghurst.acc@lwetb.ie>
2025-10-21 12:58:50 +0100

    Add files via upload

---
+++ introductiontosqlPython.py
@@ -0,0 +1,37 @@
+import sqlite3
+with sqlite3.connect("company.db") as db:
+    cursor=db.cursor()
+cursor.execute("""CREATE TABLE IF NOT EXISTS employees (
+    id integer PRIMARY KEY,
+    name text NOT NULL,
+    dept text NOT NULL,
+    salary integer) ;""")
+
+newID = input("Enter ID number: ")
+newName = input("Enter name: ")
+newDept = input("Enter department: ")
+newSalary = input("Enter salary: ")
+cursor.execute("""INSERT INTO employees(id,name,dept,salary)
+    VALUES(?,?,?,?)""",(newID,newName,newDept,newSalary))
+db.commit()
+
+cursor.execute("SELECT * FROM employees")
+print(cursor.fetchall())
+
+cursor.execute("SELECT * FROM employees")
+for x in cursor.fetchall():
+    print(x)
+    
+    cursor.execute("SELECT * FROM employees ORDER BY name")
+for x in cursor.fetchall():
+    print(x)
+    
+    
+
+db.close()
+
+
+
+
+    
+    
