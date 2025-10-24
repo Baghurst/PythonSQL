1bc644d395c3b3ce83579cba87126597c6b39338
Baghurst <23bbaghurst.acc@lwetb.ie>
2025-10-24 10:20:09 +0100

    Update introductiontosqlPython.py

--- introductiontosqlPython.py
+++ introductiontosqlPython.py
@@ -1,7 +1,7 @@
 import sqlite3
 with sqlite3.connect("company.db") as db:
     cursor=db.cursor()
-cursor.execute("""CREATE TABLE IF NOT EXISTS employees (
+'''cursor.execute("""CREATE TABLE IF NOT EXISTS employees (
     id integer PRIMARY KEY,
     name text NOT NULL,
     dept text NOT NULL,
@@ -14,17 +14,32 @@ newSalary = input("Enter salary: ")
 cursor.execute("""INSERT INTO employees(id,name,dept,salary)
     VALUES(?,?,?,?)""",(newID,newName,newDept,newSalary))
 db.commit()
-
+'''
 cursor.execute("SELECT * FROM employees")
 print(cursor.fetchall())
 
-cursor.execute("SELECT * FROM employees")
-for x in cursor.fetchall():
-    print(x)
+#cursor.execute("SELECT * FROM employees")
+#for x in cursor.fetchall():
+ #   print(x)
     
     cursor.execute("SELECT * FROM employees ORDER BY name")
 for x in cursor.fetchall():
     print(x)
+    
+    Test =   cursor.execute("SELECT * FROM employees WHERE salary>20000")
+    Test2 =  cursor.execute("SELECT * FROM employees WHERE dept='Sales'")
+    Test3 =  cursor.execute("SELECT id,name,salary FROM employees")
+    
+    whichDept = input("Enter a department: ")
+    cursor.execute("SELECT * FROM employees WHERE dept=?", [whichDept])
+    for x in cursor.fetchall():
+        print(x)
+        
+        cursor.execute("UPDATE employees SET name ='Tony' WHERE id=1")
+        db.commit()
+        
+        cursor.execute("DELETE employees WHERE id=1")
+        
     
     
 
