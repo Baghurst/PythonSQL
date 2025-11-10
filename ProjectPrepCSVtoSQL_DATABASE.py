08b679b35b13a3a9a438e99c0514ce18db776fd3
Baghurst <23bbaghurst.acc@lwetb.ie>
2025-11-10 14:41:02 +0000

    Update ProjectPrepCSVtoSQL_DATABASE.py

--- ProjectPrepCSVtoSQL_DATABASE.py
+++ ProjectPrepCSVtoSQL_DATABASE.py
@@ -2,14 +2,60 @@ import sqlite3
 with sqlite3.connect("user_email.db") as db:    
     cursor=db.cursor()
     
-while True:
+condition =0
+
+cursor.execute("SELECT Username FROM employees")
+thelist=(cursor.fetchall())
+print(thelist)
+
+while condition!=1:
+    
     userAddUsername = input("Add a username: ")
-    if userAddUsername == condition:
-        ready to add to database
+    for i in range(len(thelist)):
+        if userAddUsername == thelist[i][0]:
+            print("That username is already taken!")
+            break
+        else:
+            condition +=1
+            print('Your username is added')
+            
+        
+
+
+    
+'''while True:
+
+    
+    userAddEmail = input("Add a username: ")
+    if userAddEmail != condition:
+        #ready to add to database
         break
+    
+while True:
+
+    userAddIdentifier = input("Add a username: ")
+    if userAddIdentifier != condition:
+        #ready
+        break
+
+while True:
+    
+    userAddFirstName = input("Add a username: ")
+    if userAddFirstName != condition:
+        #ready to add to database
+        break
+    
+while True:
+
+    userAddLastName = input("Add a username: ")
+    if userAddLastName != condition:
+        #ready to add to database
+        break
+    
     else:
         print("invalid input")
-        continue  
+        continue
+    
 userAddUsername = input("Add a username: ")
 userAddEmail = input("Add an email: ")
 userAddIdentifier = input("Add an identifier: ")
@@ -23,4 +69,3 @@ db.commit()
     
 #cursor.execute("SELECT * FROM employees")
 #print(cursor.fetchall())
-    
